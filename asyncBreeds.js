8b87a0bff2fa1f59121a15d13d28077e1f78eaa6
Thainnt <thai164@gmail.com>
2021-09-22 00:16:01 +0000

    first commit Testing Asynchronous Return Values

---
+++ asyncBreeds.js
@@ -0,0 +1,22 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+
+    if (!error) functionToRunWhenThingsAreDone(data);
+    else functionToRunWhenThingsAreDone(undefined);
+  });
+
+};
+
+module.exports =breedDetailsFromFile;
+
+// const printOutCatBreed = breed => {
+//   console.log('Return Value: ', breed);
+// }
+
+// breedDetailsFromFile('Bombay', printOutCatBreed);
